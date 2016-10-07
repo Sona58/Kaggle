c5f48c5173b2c1ea329bb04901d802df2d9e7b57
Sonali Chawla <s2sonali@gmail.com>
2016-10-07 21:17:53 +0530

    Update titanic.R

--- titanic.R
+++ titanic.R
@@ -1,52 +1,40 @@
-train <- read.csv("train.csv",header=T)
+library(party)
+library(rpart)
 
-# Import the testing set: test
-test <- read.csv("test.csv",header=T)
+train <- read.csv("../input/train.csv")
+test  <- read.csv("../input/test.csv")
 
-# Print train and test to the console
-train
-test
+test$Survived <- NA
+combi <- rbind(train, test)
+combi$Name <- as.character(combi$Name)
+combi$Title <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+combi$Title <- sub(' ', '', combi$Title)
+combi$Title[combi$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+combi$Title <- factor(combi$Title)
+combi$FamilySize <- combi$SibSp + combi$Parch + 1
+combi$Surname <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+combi$FamilyID <- paste(as.character(combi$FamilySize), combi$Surname, sep="")
+combi$FamilyID[combi$FamilySize <= 2] <- 'Small'
+famIDs <- data.frame(table(combi$FamilyID))
+famIDs <- famIDs[famIDs$Freq <= 2,]
+combi$FamilyID[combi$FamilyID %in% famIDs$Var1] <- 'Small'
+combi$FamilyID <- factor(combi$FamilyID)
+Agefit <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + FamilySize,data=combi[!is.na(combi$Age),], method="anova")
+combi$Age[is.na(combi$Age)] <- predict(Agefit, combi[is.na(combi$Age),])
+combi$Embarked[c(62,830)] = "S"
+combi$Embarked <- factor(combi$Embarked)
+combi$Fare[1044] <- median(combi$Fare, na.rm=TRUE)
+combi$FamilyID2 <- combi$FamilyID
+combi$FamilyID2 <- as.character(combi$FamilyID2)
+combi$FamilyID2[combi$FamilySize <= 3] <- 'Small'
+combi$FamilyID2 <- factor(combi$FamilyID2)
+train <- combi[1:891,]
+test <- combi[892:1309,]
 
-# Your train and test set are still loaded
-str(train)
-str(test)
-
-# Survival rates in absolute numbers
-table(train$Survived)
-
-# Survival rates in proportions
-prop.table(table(train$Survived))
-
-# Two-way comparison: Sex and Survived
-table(train$Sex,train$Survived)
-
-# Two-way comparison: row-wise proportions
-prop.table(table(train$Sex,train$Survived),margin=1)
-
-# Your train and test set are still loaded in
-str(train)
-str(test)
-
-# Create the column child, and indicate whether child or no child
-train$Child <- NA
-train$Child[train$Age < 18] <- 1
-train$Child[train$Age >= 18] <- 0
-
-# Two-way comparison
-prop.table(table(train$Child,train$Survived),margin=1)
-
-# Your train and test set are still loaded in
-str(train)
-str(test)
-
-# Copy of test
-test_one <- test
-
-# Initialize a Survived column to 0
-test_one$Survived<-0
-
-# Set Survived to 1 if Sex equals "female"
-test_one$Survived[test_one$Sex == "female"] <-1
-
-test_two<-test_one[c(1,12)]
-write.csv(test_two,"my_solution.csv",row.names = F)
+set.seed(415)
+fit <- cforest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare +Embarked + Title + FamilySize + FamilyID,data = train, controls=cforest_unbiased(ntree=2000, mtry=3))
+Prediction <- predict(fit, test, OOB=TRUE, type = "response")
+results <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+write.csv(results, file = "results.csv", row.names = FALSE)
